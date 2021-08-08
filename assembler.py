3a594371003368ddaf22fb8d73690398a1bfaff7
Krishnam Omar <omegakrish13@gmail.com>
2021-08-08 22:43:58 +0530

    define

--- assembler.py
+++ assembler.py
@@ -1,1 +1,44 @@
-# jjjmk
+def Addition(r1, r2, r3):
+    pass
+# to print the opcode
+
+def Subtraction(r1, r2, r3):
+    pass
+
+def Move_Immediate(r1, x):
+    #x is int
+    pass
+
+def Move_Register(r1, r2):
+    pass
+
+def Load(r1, mem):
+    # mem is memory address
+    pass
+
+def Store(r1, mem):
+    pass
+
+def Multiply(r1, r2, r3):
+    pass
+
+def Divide(r3, r4):
+    # quotent in r0 
+    # remainder in r1
+    pass
+
+def Right_Shift(r1, x):
+    #x is int
+    pass
+
+def Left_Shift(r1, x):
+    pass
+
+def XOR(r1, r2, r3):
+    pass
+
+def OR(r1, r2, r3):
+    pass
+
+def AND(r1, r2, r3):
+    pass
