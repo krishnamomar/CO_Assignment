a7a21d434c0bbc4680c8df9abae6411bb5391428
harshit garg <harshit30062002@gmail.com>
2021-08-11 23:53:55 +0530

    edited first 6 functions

--- assembler.py
+++ assembler.py
@@ -4,26 +4,86 @@ class Register:
 
 
 
-def Addition(r1, r2, r3):
-    pass
+def Addition(r1, r2, r3): #Performs reg1 = reg2+ reg3. If the computation overflows, then the overflow flag is set 
+    a="00000"
+    b="00"
+    c="001"
+    d="010"
+    e="011"
+    f=a+b+c+d+e
+    return f
+
+
+    # if overflow-----flag(1,0,0,0)
+    # pass
+    #if not overflows----flag(0,0,0,0)
 # to print the opcode
 
-def Subtraction(r1, r2, r3):
-    pass
+def Subtraction(r1, r2, r3):  #Performs reg1 = reg2- reg3. In case reg3> reg2, 0 is written to reg1 and overflow flag is set. 
+    # if r3>r2:
+        # r1=0
+        #overflow----flag(1,0,0,0)
+    # r1=r2-r3
+    a="00001"
+    b="00"
+    c="001"
+    d="010"
+    e="011"
+    f=a+b+c+d+e
+    return f
+    
+    # pass
 
-def Move_Immediate(r1, x):
+def Move_Immediate(r1, x): #Performs reg1 = $Imm where Imm is a 8 bit value.
+    # r1=x
     #x is int
-    pass
+    a="00010"
+    # b="00"
+    b="001"
+    # d="010"
+    # e="011"
+    c=x
+    # f=a+b+c+d+e
+    f=a+b
+    return f
+
+    # pass
 
 def Move_Register(r1, r2):
-    pass
+    # r1=r2
+    a="00011"
+    b="00000"
+    c="001"
+    d="010"
+    # e="011"
+    f=a+b+c+d
+    return f
+    # pass
 
 def Load(r1, mem):
     # mem is memory address
-    pass
+    #load value at address mem to r1
+    a="00100"
+    # b="00"
+    b="001"
+    # d="010"
+    # e="011"
+    c=mem
+    f=a+b+c
+    return f
+    # pass
 
 def Store(r1, mem):
-    pass
+    #Stores data from reg1 to mem_addr.
+    a="00101"
+    # b="00"
+    b="001"
+    # d="010"
+    # e="011"
+    c=mem
+    f=a+b+c
+    return f
+    # pass
 
 def Multiply(r1, r2, r3):
     pass
