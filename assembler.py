2a54ab6ce7c7a77102f9020e63c5459f1fb6080d
Krishnam Omar <omegakrish13@gmail.com>
2021-08-15 15:16:33 +0530

    error handling

--- assembler.py
+++ assembler.py
@@ -1108,9 +1108,19 @@ for instr in range(len(instructions)):
         OutputFile = ["Error in line number: " + str(instr+1)]
         break
      
-if "1001100000000000" not in OutputFile:
-    OutputFile = ["Error: hlt not present"]
+if len(OutputFile)==1:
+    chk = OutputFile[0].split()
+    if "Error" in chk:
+        print(OutputFile[0])
+    elif OutputFile[0]=="1001100000000000":
+        print("1001100000000000")
+    else:
+        print("Error: hlt not present")
 
-for bb in OutputFile:     # for printing the Output
-    print(bb)
+elif "1001100000000000" not in OutputFile:
+     OutputFile = ["Error: hlt not present"]
+
+else:
+    for bb in OutputFile:     # for printing the Output
+        print(bb)
         
